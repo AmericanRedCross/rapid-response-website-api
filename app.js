90e0ecbad9892991ae7fcc9f4e1f14d38692dc44
danbjoseph <danbjoseph@gmail.com>
2016-03-17 14:39:15 -0400

    removing cors package

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var path = require('path');
 var localConfig = require('./config');
-var cors = require('cors');
+//var cors = require('cors');
 var app = express();
 
 app.use('/docs', express.static(path.join(localConfig.application.dboxpath,localConfig.application.prjfolder)));
 
-app.use(cors());
+app.use(function(req,res,next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var FolderParse = require('./routes/folderParse.js');
 var folderparse = new FolderParse();
 
 app.get('/files',function(req,res,next) {
 		folderparse.retrieveFiles(function(err,list){
-			res.json({data: list});
+			res.send(data);
 		})
 })
 
